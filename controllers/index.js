f04784ce102cfcbc66d070663146f353dfa76b05
Cara R <cara.rosa.02@gmail.com>
2023-11-13 10:22:27 -0600

    added required routes and used routes

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const blogPostRoute = require('./blogPostRoute');
+
+router.use('/', blogPostRoute);
+router.use('/api', apiRoutes);
