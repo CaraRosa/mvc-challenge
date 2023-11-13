fa9b89a8a533a063489c0463a024c07308c08da5
Cara R <cara.rosa.02@gmail.com>
2023-11-13 10:39:39 -0600

    commented out code

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const blogPostRoute = require('./blogPostRoute');
+// const apiRoutes = require('./api');
+// const blogPostRoute = require('./blogPostRoute');
 
-router.use('/', blogPostRoute);
-router.use('/api', apiRoutes);
+// router.use('/', blogPostRoute);
+// router.use('/api', apiRoutes);
